8d9c2abb1e474edb9ff5c4459548e2ab556f9f9a
nsw99 <133620206+nsw99@users.noreply.github.com>
2023-09-19 09:08:06 +0900

    09-18

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+const StyledMain = styled.main`
+    padding-top: 56px;
+    display: flex;
+`
+const Home = () => {
+    return <StyledMain>
+    <h1>HomeTest</h1>
+    </StyledMain>
+}
+export default Home;
